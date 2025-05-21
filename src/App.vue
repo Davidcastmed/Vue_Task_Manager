b2f8f77c2fba98377c74c638089c95878acdacfa
David Castillo <castillo.medina.david@gmail.com>
2025-05-21 13:07:18 +0200

    Hardcoded task are deleted

--- src/App.vue
+++ src/App.vue
@@ -15,50 +15,50 @@ store.$subscribe((mutation, state) => {
 const appName = "Tasks Manager";
 
 
-let tasks= reactive([
-    {
-      name: "Website design",
-      description: "Define the style guide, branding and create the webdesign on Figma.",
-      completed: true,
-      id:1
-    },
-    {
-      name: "Website development",
-      description: "Develop the portfolio website using Vue JS.",
-      completed: false,
-      id:2
-    },
-    {
-      name: "Hosting and infrastructure",
-      description: "Define hosting, domain and infrastructure for the portfolio website.",
-      completed: false,
-      id:3
-    },
-    {
-      name: "Composition API",
-      description: "Learn how to use the composition API and how it compares to the options API.",
-      completed: true,
-      id:4
-    },
-    {
-      name: "Pinia",
-      description: "Learn how to setup a store using Pinia.",
-      completed: true,
-      id:5
-    },
-    {
-      name: "Groceries",
-      description: "Buy rice, apples and potatos.",
-      completed: false,
-      id:6
-    },
-    {
-      name: "Bank account",
-      description: "Open a bank account for my freelance business.",
-      completed: false,
-      id:7
-    }
-]);
+// let tasks= reactive([
+//     {
+//       name: "Website design",
+//       description: "Define the style guide, branding and create the webdesign on Figma.",
+//       completed: true,
+//       id:1
+//     },
+//     {
+//       name: "Website development",
+//       description: "Develop the portfolio website using Vue JS.",
+//       completed: false,
+//       id:2
+//     },
+//     {
+//       name: "Hosting and infrastructure",
+//       description: "Define hosting, domain and infrastructure for the portfolio website.",
+//       completed: false,
+//       id:3
+//     },
+//     {
+//       name: "Composition API",
+//       description: "Learn how to use the composition API and how it compares to the options API.",
+//       completed: true,
+//       id:4
+//     },
+//     {
+//       name: "Pinia",
+//       description: "Learn how to setup a store using Pinia.",
+//       completed: true,
+//       id:5
+//     },
+//     {
+//       name: "Groceries",
+//       description: "Buy rice, apples and potatos.",
+//       completed: false,
+//       id:6
+//     },
+//     {
+//       name: "Bank account",
+//       description: "Open a bank account for my freelance business.",
+//       completed: false,
+//       id:7
+//     }
+// ]);
 
 
 
@@ -78,7 +78,7 @@ let tasks= reactive([
          + Add Task
         </button>
       </div>
-      <input type ="text" v-model="appName">
+      <!-- <input type ="text" v-model="appName"> -->
     </div>
     
     <Filter/>
